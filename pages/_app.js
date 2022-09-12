80d3b15468b02e389b545154e518e2db7b750b59
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-09-12 18:39:55 +0530

    checking the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
+import { StateProvider } from "./StateProvider";
+import reducer, { initialState } from "./reducer";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <StateProvider reducer={reducer} initialState={initialState}>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </StateProvider>
   );
 }
 
