53e56c6c6c728fa525098a728d19b86aad830688
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-09-12 14:29:28 +0530

    Cart Quantity button made

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,15 @@
 import Link from "next/link";
 import { AiOutlineShoppingCart } from "react-icons/ai";
-function Navbar() {
+function Navbar({cartQty}) {
   return (
-    <div className="container m-auto h-28 bg-white sticky top-0 py-8 px-4 flex flex-row items-center">
+    <div className="container m-auto bg-white sticky top-0 py-6 px-5 flex flex-row items-center">
       <Link href="/">
         <a>
           <img src="/logo.png" alt="Wasatch Ski Company" />
         </a>
       </Link>
-      <div className="flex-1 flex flex-row justify-end space-x-8">
-        <div className="flex flex-row space-x-4 font-light text-xl text-black">
+      <div className="flex-1 flex flex-row justify-end space-x-7 items-center">
+        <div className="flex flex-row space-x-5 font-light text-normal text-black">
           <Link href="/">
             <a>Home</a>
           </Link>
@@ -23,12 +23,15 @@ function Navbar() {
             <a>Contact Us</a>
           </Link>
         </div>
-        <div>
+        <div className="relative">
           <Link href="/cart">
             <a>
-              <AiOutlineShoppingCart className="text-3xl" />
+              <AiOutlineShoppingCart className="text-3xl"/>
             </a>
           </Link>
+          <div className="border-2 box-border px-1.5 py-0.5 font-semibold text-xs items-center justify-center border-white text-white bg-indigo-500 rounded-full absolute -top-2 -right-3">
+            {cartQty ? cartQty : 0}
+          </div>
         </div>
       </div>
     </div>
