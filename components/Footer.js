2928253a178f6ad359c5d02f3ed94bb094d6bfc5
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-09-12 18:53:02 +0530

    footer made toggling

--- components/Footer.js
+++ components/Footer.js
@@ -1,27 +1,41 @@
 import Link from "next/link";
 import { AiOutlineTwitter } from "react-icons/ai";
-import {FaFacebook, FaFacebookF, FaLinkedinIn} from "react-icons/fa";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import { useStateValue } from "../StateProvider";
 function Footer() {
+  const [{ mode }, dispatch] = useStateValue();
   return (
-    <div className="container px-5 m-auto flex flex-row items-center mt-16">
+    <div
+      className={`container px-5 m-auto flex flex-row items-center mt-16 ${
+        mode === "light" ? "bg-white" : "bg-black"
+      }`}
+    >
       <img src="/logo-footer.png" alt="" />
-      <div className="border-l-2 flex flex-row px-4 mx-4 py-1 text-slate-500 text-sm">
+      <div
+        className={`border-l-2 flex flex-row px-4 mx-4 py-1 ${
+          mode === "light" ? "text-slate-500" : "text-white"
+        } text-sm`}
+      >
         &copy;&nbsp;{new Date().getFullYear()}&nbsp;Wasatch Ski Company
       </div>
-      <div className="flex-1 flex flex-row justify-end space-x-2 text-slate-500">
+      <div
+        className={`flex-1 flex flex-row justify-end space-x-2 ${
+          mode === "light" ? "text-slate-500" : "text-white"
+        }`}
+      >
         <Link href="https://twitter.com">
           <a>
-            <AiOutlineTwitter className="text-xl"/>
+            <AiOutlineTwitter className="text-xl" />
           </a>
         </Link>
         <Link href="https://linkedin.com/in">
           <a>
-            <FaLinkedinIn className="text-xl"/>
+            <FaLinkedinIn className="text-xl" />
           </a>
         </Link>
         <Link href="https://facebook.com">
           <a>
-            <FaFacebookF className="text-xl"/>
+            <FaFacebookF className="text-xl" />
           </a>
         </Link>
       </div>
