668f8e451ec156e8e37271097274edf4f07901ed
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-09-12 15:26:46 +0530

    Deployment bug fixes

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 function Home() {
   return (
     <div className="container m-auto flex flex-col items-center px-4">
-      <Image
+      <img
         src="/skis.jpeg"
         alt="hero banner"
         className="w-[100vw] object-none object-center h-[55vh] mb-10"
@@ -15,11 +13,13 @@ function Home() {
         <font className="text-3xl py-1 font-medium">Wasatch Ski Company</font>
         <font className="mt-4 w-[58vw] text-center font-normal">
           A pair of skis are the ultimate transportation to freedom. Skiing is a
-          dance, and the mountain always leads. You can't buy happiness, but you
-          can buy a ski pass.
+          dance, and the mountain always leads. You can&apos;t buy happiness,
+          but you can buy a ski pass.
         </font>
       </div>
-      <button className="bg-indigo-500 rounded px-8 py-2 text-white my-6 text-lg">Shop Now</button>
+      <button className="bg-indigo-500 rounded px-8 py-2 text-white my-6 text-lg">
+        Shop Now
+      </button>
     </div>
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,11 @@
-import Image from "next/image";
 import Link from "next/link";
 import { AiOutlineShoppingCart } from "react-icons/ai";
-function Navbar({cartQty}) {
+function Navbar({ cartQty }) {
   return (
     <div className="container m-auto bg-white py-6 px-5 flex flex-row items-center">
       <Link href="/">
         <a>
-          <Image src="/logo.png" alt="Wasatch Ski Company" />
+          <img src="/logo.png" alt="Wasatch Ski Company" />
         </a>
       </Link>
       <div className="flex-1 flex flex-row justify-end space-x-7 items-center">
@@ -27,7 +26,7 @@ function Navbar({cartQty}) {
         <button className="relative">
           <Link href="/cart">
             <a>
-              <AiOutlineShoppingCart className="text-3xl"/>
+              <AiOutlineShoppingCart className="text-3xl" />
             </a>
           </Link>
           <div className="border-2 box-border px-1.5 py-0.5 font-semibold text-xs items-center justify-center border-white text-white bg-indigo-500 rounded-full absolute -top-2 -right-3">
